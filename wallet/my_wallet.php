aac8879436d6678e81a6c7780b7de5d6ea3fac40
Arisetty Varunraj <varunrajbbl@gmail.com>
2018-04-30 23:52:50 +0530

    Add files via upload

---
+++ wallet/my_wallet.php
@@ -0,0 +1,39 @@
+<?php include('header.php');?>
+</div>
+<div class="content">
+	<div class="wrap">
+		<div class="content-top">
+			<h3>My Wallet</h3>
+			
+			<ul class="group" id="example-one">
+              
+            <li><a href="add_wallet.php">Add Money</a></li>            
+            <li><a href="update_wallet.php">Update Wallet</a></li> 
+            </ul>
+			<?php
+          	 
+          	 $qry2=mysqli_query($con,"select * from  tbl_wallet ");
+		
+          	  while($m=mysqli_fetch_array($qry2))
+                   {
+                    ?>
+                    
+                    <div class="col_1_of_4 span_1_of_4">
+					<div class="imageRow">
+						  	<h3>Wallet Balance</h3>
+						  	<div class="movie-text">
+						  		<h4 class="h-text"><?php echo $m['wallet_bal']?></h4>
+						  		
+						  		
+						  	</div>
+		  				</div>
+		  		</div>
+		  		
+  	    <?php
+  	    	}
+  	    	?>
+			
+			</div>
+				<div class="clear"></div>		
+			</div>
+			<?php include('footer.php');?>
